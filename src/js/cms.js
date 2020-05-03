198ba66a22e9303287ea73a437b82c3bd25f891a
Alexander Schetnikov <go.sanyaaa@gmail.com>
2020-05-03 14:57:36 +0700

    Modify CMS

--- src/js/cms.js
+++ src/js/cms.js
@@ -1,8 +1,8 @@
-import React from "react";
-import CMS from "netlify-cms-app";
+// import React from "react";
+// import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
+// import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.css";
 
 // import HomePreview from "./cms-preview-templates/home";
 // import PostPreview from "./cms-preview-templates/post";
@@ -10,10 +10,10 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 // import ValuesPreview from "./cms-preview-templates/values";
 // import ContactPreview from "./cms-preview-templates/contact";
 
-CMS.registerPreviewStyle(styles, { raw: true });
+// CMS.registerPreviewStyle(styles, { raw: true });
 // CMS.registerPreviewTemplate("home", HomePreview);
 // CMS.registerPreviewTemplate("post", PostPreview);
 // CMS.registerPreviewTemplate("products", ProductsPreview);
 // CMS.registerPreviewTemplate("values", ValuesPreview);
 // CMS.registerPreviewTemplate("contact", ContactPreview);
-CMS.init();
+// CMS.init();
